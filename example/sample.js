8f3f8b0d5be38bcae50ed0b3cfb69e7051bbec84
Alessandro Valenti <valenti1234@gmail.com>
2020-09-02 12:08:27 +0000

    fix close

--- example/sample.js
+++ example/sample.js
@@ -1,58 +1,93 @@
+  const faker = require('faker');
+
 const MongoTail = require("../lib/mongo-q-tail.js");
+//const dbUrl = 'mongodb://mongodb_host:27017'
+//const dbUrl = 'mongodb://172.16.14.136:27017'
 
-let options = {
-    dbUrl: "mongodb://localhost:27017",
-    dbName: 'tailtest',
-    collection: 'q_submit',
-    filter: { processed: false },
-    throttle: 200,
-    autoStart:true,
-};
+//const dbUrl = 'mongodb://172.16.14.136:27017';
+//const dbUrl  = "mongodb://172.16.14.136:27017,//172.16.12.136:27017?replicaSet=rs0";
+//const dbUrl  = 'mongodb://172.16.12.136:27017?replicaSet=rs0&slaveOk=true';
 
-const mt = new MongoTail(options);
+//const dbUrl  = 'mongodb://172.16.14.136:27017?replicaSet=rs0';
+//const dbUrl  = 'mongodb://repl12:27017,repl14:27017';
 
-mt.on('data', function(data) {
-    console.log('get a doc', data._id,data.name);
-    mt.getQueue().then(res => {
-        console.log('queue size', res);
-        mt.updateDoc({
-            filter: {_id: data._id},
-            update: { processed: true }
+//-------------
+//const dbUrl  = 'mongodb://repl12:27017?replicaSet=rs0';
+const dbUrl = 'mongodb://mongo-tail:27017';
+
+function startTail() {
+    let options = {
+        dbUrl: dbUrl,
+        dbName: 'tailtest',
+        collection: 'q_submit',
+        filter: { processed: false },
+        processedUpdate: { processed: true },
+        throttle: 30,
+        autoStart: true,
+        alertCount: 10,
+        debugInfo: true,
+        indexList: ["processed", "address.zipcode", "accountHistory.type"],
+        size: 10240000,
+        drop: true,
+
+    };
+
+    const mt = new MongoTail(options);
+
+    mt.on('error', function(data) {
+        console.error('Event error', data);
+    })
+    
+    mt.on('start', function(data) {
+        console.log('MongoTail started', data);
+
+        function insertOne() {
+            return new Promise(function(fulfilled, rejected) {
+                var randomCard = faker.helpers.createCard(); // random contact card containing many properties
+                let item = randomCard;
+                item.processed = false;
+                console.log('gen', item.name)
+                mt.insertDoc(item).then(fulfilled).catch(rejected);
+            });
+        }
+
+        function loopInsert() {
+            let high = 118;
+            let low = 1;
+
+            setTimeout(() => {
+                insertOne().then(loopInsert).catch(err => {
+                    console.error('insertOne error', err);
+                });
+            }, Math.random() * (high - low) + low);
+        }
+
+        setTimeout(loopInsert,10*1000);
+
+    })
+
+    mt.on('data', function(data) {
+        console.log('get a doc', data._id, data.name);
+        mt.markAsProcessed({
+            filter: { _id: data._id }
         }).then(res => {
             mt.nextDoc();
         }).catch(console.error)
+    })
 
-    }).catch(console.error)
-})
-
-
-function Generator() {
-    const MongoClient = require('mongodb').MongoClient;
-    const faker = require('faker');
-
-
-    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-        if (err)
-            throw err;
-
-        let coll = client.db('tailtest').collection('q_submit'); //open collection
+    mt.on('alert', function(data) {
+        console.log('get an alert', data);
+        mt.options.throttle = 1;
+        setTimeout(() => {
+            mt.options.throttle = 1000;
+        }, 10 * 1000)
+    })
 
 
-        setInterval(() => {
-
-            var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-
-            let item = randomCard;
-            item.processed = false;
-
-            console.log('gen', item.name)
-            coll.insertOne(item);
-
-        }, 300)
-
-
-    });
 
 }
 
-let gn = new Generator();
+
+setTimeout(() => {
+    startTail();
+}, 1 * 1000)
